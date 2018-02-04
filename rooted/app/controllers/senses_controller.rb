e559b2b64fef882978982ebe7a81ecbe5b80195b
eyingling02 <eyingling02@gmail.com>
2018-02-03 16:37:02 -0800

    Update views for each sense

--- rooted/app/controllers/senses_controller.rb
+++ rooted/app/controllers/senses_controller.rb
@@ -1,17 +1,27 @@
 class SensesController < ApplicationController
 
-  def five
+  def show
+
+    #gets params from url
+
+    @sense = Sense.find(params[:id]) #define instance variable for view
+    case @sense.id
+    when 5
+      render 'senses/five'
+    when 4
+      render 'senses/four'
+    when 3
+      render 'senses/three'
+    when 2
+      render 'senses/two'
+    when 1
+      render 'senses/one'
+    end
+
+    # render 'senses/show' #show single sense view
+
   end
 
-  def four
-  end
-
-  def three
-  end
-
-  def two
-  end
-
-  def one
+  def index
   end
 end
