399107c49f1b4c9d07662649b0e6a76ddf1a0023
Camille Villa <cvilla100@gmail.com>
2018-02-03 16:42:47 -0800

    Update senses controller for grounding exercise

--- rooted/app/controllers/senses_controller.rb
+++ rooted/app/controllers/senses_controller.rb
@@ -1,2 +1,27 @@
 class SensesController < ApplicationController
+  before_action :set_sense, only: [:show, :edit, :update, :destroy]
+
+  # GET /tests
+  # GET /tests.json
+  def index
+    @senses = Sense.all
+    @sensory_details = SensoryDetail.all
+  end
+
+  # GET /senses/1
+  # GET /senses/1.json
+  def show
+    @sensory_details = @sense.sensory_details
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_sense
+      @sense = Sense.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def sense_params
+      params.fetch(:sense, {})
+    end
 end
